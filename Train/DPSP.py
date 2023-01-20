11a7f6743595d283755c906e229151180a7e4823
raziyeh masumshah <r_masumshah@sbu.ac.ir>
2023-01-21 01:28:36 +0330

    Add files via upload

---
+++ Train/DPSP.py
@@ -0,0 +1,297 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from NLPProcess import NLPProcess
+from numpy.random import seed
+seed(1)
+import csv
+import sqlite3
+import time
+import numpy as np
+import pandas as pd
+import pickle
+from pandas import DataFrame
+from sklearn.model_selection import KFold
+from sklearn.decomposition import PCA
+from sklearn.metrics import auc
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import precision_recall_curve
+from sklearn.preprocessing import label_binarize
+from keras.models import Model
+from keras.layers import Dense, Dropout, Input, Activation, BatchNormalization
+from keras.callbacks import EarlyStopping
+#-----------------------------------------------------------------------------------------
+def DNN():
+    train_input = Input(shape=(new_feature.shape[1],), name='Inputlayer')
+    train_in = Dense(512, activation='relu')(train_input)
+    train_in = BatchNormalization()(train_in)
+    train_in = Dropout(droprate)(train_in)
+    train_in = Dense(256, activation='sigmoid')(train_in)
+    train_in = BatchNormalization()(train_in)
+    train_in = Dropout(droprate)(train_in)
+    train_in = Dense(128, activation='sigmoid')(train_in)
+    train_in = BatchNormalization()(train_in)
+    train_in = Dropout(droprate)(train_in)
+    train_in = Dense(event_num)(train_in)
+    out = Activation('softmax')(train_in)
+    model = Model(train_input, out)
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
+#--------------------------------------------------------------------------------------------
+def prepare(df_drug, feature_list, vector_size,mechanism,action,drugA,drugB):
+    d_label = {}
+    d_feature = {}
+    # Transfrom the interaction event to number
+    # Splice the features
+    d_event=[]
+    for i in range(len(mechanism)):
+        d_event.append(mechanism[i]+" "+action[i])
+    label_value = 0
+    count={}
+    for i in d_event:
+        if i in count:
+            count[i]+=1
+        else:
+            count[i]=1
+    list1 = sorted(count.items(), key=lambda x: x[1],reverse=True)
+    for i in range(len(list1)):
+        d_label[list1[i][0]]=i
+    vector = np.zeros((len(np.array(df_drug['name']).tolist()), 0), dtype=float)
+    for i in feature_list:
+        vector = np.hstack((vector, feature_vector(i, df_drug, vector_size)))
+    # Transfrom the drug ID to feature vector
+    for i in range(len(np.array(df_drug['name']).tolist())):
+        d_feature[np.array(df_drug['name']).tolist()[i]] = vector[i]
+    # Use the dictionary to obtain feature vector and label
+    new_feature = []
+    new_label = []
+    name_to_id = {}
+    for i in range(len(d_event)):
+        new_feature.append(d_feature[drugA[i]]+ d_feature[drugB[i]])
+        new_label.append(d_label[d_event[i]])
+    new_feature = np.array(new_feature)
+    new_label = np.array(new_label)
+    return (new_feature, new_label, event_num)
+#----------------------------------------------------------------------------------------
+def feature_vector(feature_name, df, vector_size):
+    # df are the 572 kinds of drugs
+    # Jaccard Similarity
+    def Jaccard(matrix):
+        matrix = np.mat(matrix)
+        numerator = matrix * matrix.T
+        denominator = np.ones(np.shape(matrix)) * matrix.T + matrix * np.ones(np.shape(matrix.T)) - matrix * matrix.T
+        return numerator / denominator
+
+    all_feature = []
+    drug_list = np.array(df[feature_name]).tolist()
+    # Features for each drug, for example, when feature_name is target, drug_list=["P30556|P05412","P28223|P46098|……"]
+    for i in drug_list:
+        for each_feature in i.split('|'):
+            if each_feature not in all_feature:
+                all_feature.append(each_feature)  # obtain all the features
+    feature_matrix = np.zeros((len(drug_list), len(all_feature)), dtype=float)
+    df_feature = DataFrame(feature_matrix, columns=all_feature)  # Consrtuct feature matrices
+    for i in range(len(drug_list)):
+        for each_feature in df[feature_name].iloc[i].split('|'):
+            df_feature[each_feature].iloc[i] = 1
+    sim_matrix = Jaccard(np.array(df_feature))
+
+    sim_matrix1 = np.array(sim_matrix)
+    count = 0
+    pca = PCA(n_components=vector_size)  # PCA dimension
+    pca.fit(sim_matrix)
+    sim_matrix = pca.transform(sim_matrix)
+    return sim_matrix
+#-------------------------------------------------------------------------------------------------
+def get_index(label_matrix, event_num, seed, CV):
+    index_all_class = np.zeros(len(label_matrix))
+    for j in range(event_num):
+        index = np.where(label_matrix == j)
+        kf = KFold(n_splits=CV, shuffle=True, random_state=seed)
+        k_num = 0
+        for train_index, test_index in kf.split(range(len(index[0]))):
+            index_all_class[index[0][test_index]] = k_num
+            k_num += 1
+
+    return index_all_class
+#------------------------------------------------------------------------------------------------
+def cross_validation(feature_matrix, label_matrix, event_num, seed, CV):
+    all_eval_type = 11
+    result_all = np.zeros((all_eval_type, 1), dtype=float)
+    each_eval_type = 6
+    result_eve = np.zeros((event_num, each_eval_type), dtype=float)
+    y_true = np.array([])
+    y_pred = np.array([])
+    y_score = np.zeros((0, event_num), dtype=float)
+    index_all_class = get_index(label_matrix, event_num, seed, CV)
+    matrix = []
+    if type(feature_matrix) != list:
+        matrix.append(feature_matrix)
+        feature_matrix = matrix
+    for k in range(CV):
+        train_index = np.where(index_all_class != k)
+        test_index = np.where(index_all_class == k)
+        pred = np.zeros((len(test_index[0]), event_num), dtype=float)
+        # dnn=DNN()
+        for i in range(len(feature_matrix)):
+            x_train = feature_matrix[i][train_index]
+            x_test = feature_matrix[i][test_index]
+            y_train = label_matrix[train_index]
+            # one-hot encoding
+            y_train_one_hot = np.array(y_train)
+            y_train_one_hot = (np.arange(y_train_one_hot.max() + 1) == y_train[:, None]).astype(dtype='float32')
+            y_test = label_matrix[test_index]
+            # one-hot encoding
+            y_test_one_hot = np.array(y_test)
+            y_test_one_hot = (np.arange(y_test_one_hot.max() + 1) == y_test[:, None]).astype(dtype='float32')
+            
+            dnn = DNN()
+            early_stopping = EarlyStopping(monitor='val_loss', patience=10, verbose=0, mode='auto')
+            dnn.fit(x_train, y_train_one_hot, batch_size=128, epochs=100, validation_data=(x_test, y_test_one_hot),
+                    callbacks=[early_stopping])
+            pred += dnn.predict(x_test)
+                
+        pred_score = pred / len(feature_matrix)
+        pred_type = np.argmax(pred_score, axis=1)
+        y_true = np.hstack((y_true, y_test))
+        y_pred = np.hstack((y_pred, pred_type))
+        y_score = np.row_stack((y_score, pred_score))
+    result_all, positive_negative, result_eve = evaluate(y_pred, y_score, y_true, event_num)
+    return result_all, positive_negative, result_eve
+#-------------------------------------------------------------------------------------------------------------
+def evaluate(pred_type, pred_score, y_test, event_num):
+    all_eval_type = 11
+    result_all = np.zeros((all_eval_type, 1), dtype=float)
+    each_eval_type = 6
+    result_eve = np.zeros((event_num, each_eval_type), dtype=float)
+    y_one_hot = label_binarize(y_test, np.arange(event_num))
+    pred_one_hot = label_binarize(pred_type, np.arange(event_num))
+
+    precision, recall, th = multiclass_precision_recall_curve(y_one_hot, pred_score)
+
+    result_all[0] = accuracy_score(y_test, pred_type)
+    result_all[1] = roc_aupr_score(y_one_hot, pred_score, average='micro')
+    result_all[2] = roc_aupr_score(y_one_hot, pred_score, average='macro')
+    result_all[3] = roc_auc_score(y_one_hot, pred_score, average='micro')
+    result_all[4] = roc_auc_score(y_one_hot, pred_score, average='macro')
+    result_all[5] = f1_score(y_test, pred_type, average='micro')
+    result_all[6] = f1_score(y_test, pred_type, average='macro')
+    result_all[7] = precision_score(y_test, pred_type, average='micro')
+    result_all[8] = precision_score(y_test, pred_type, average='macro')
+    result_all[9] = recall_score(y_test, pred_type, average='micro')
+    result_all[10] = recall_score(y_test, pred_type, average='macro')
+    positive_negative= np.hstack(self_metric_calculate(y_test, pred_type))
+    for i in range(event_num):
+        result_eve[i, 0] = accuracy_score(y_one_hot.take([i], axis=1).ravel(), pred_one_hot.take([i], axis=1).ravel())
+        result_eve[i, 1] = roc_aupr_score(y_one_hot.take([i], axis=1).ravel(), pred_one_hot.take([i], axis=1).ravel(),
+                                          average=None)
+        result_eve[i, 2] = roc_auc_score(y_one_hot.take([i], axis=1).ravel(), pred_one_hot.take([i], axis=1).ravel(),
+                                         average=None)
+        result_eve[i, 3] = f1_score(y_one_hot.take([i], axis=1).ravel(), pred_one_hot.take([i], axis=1).ravel(),
+                                    average='binary')
+        result_eve[i, 4] = precision_score(y_one_hot.take([i], axis=1).ravel(), pred_one_hot.take([i], axis=1).ravel(),
+                                           average='binary')
+        result_eve[i, 5] = recall_score(y_one_hot.take([i], axis=1).ravel(), pred_one_hot.take([i], axis=1).ravel(),
+                                        average='binary')
+    return [result_all, positive_negative, result_eve]
+#-------------------------------------------------------------------------------------------------------------
+def self_metric_calculate(y_true, pred_type):
+    y_true = y_true.ravel()
+    y_pred = pred_type.ravel()
+    if y_true.ndim == 1:
+        y_true = y_true.reshape((-1, 1))
+    if y_pred.ndim == 1:
+        y_pred = y_pred.reshape((-1, 1))
+    y_true_c = y_true.take([0], axis=1).ravel()
+    y_pred_c = y_pred.take([0], axis=1).ravel()
+    TP = 0
+    TN = 0
+    FN = 0
+    FP = 0
+    for i in range(len(y_true_c)):
+        if (y_true_c[i] == 1) and (y_pred_c[i] == 1):
+            TP += 1
+        if (y_true_c[i] == 1) and (y_pred_c[i] == 0):
+            FN += 1
+        if (y_true_c[i] == 0) and (y_pred_c[i] == 1):
+            FP += 1
+        if (y_true_c[i] == 0) and (y_pred_c[i] == 0):
+            TN += 1
+    print("TP=", TP, "FN=", FN, "FP=", FP, "TN=", TN)
+    #return (TP / (TP + FP), TP / (TP + FN))
+    return(TP, TN, FP, FN)
+#--------------------------------------------------------------------------------------------
+def multiclass_precision_recall_curve(y_true, y_score):
+    y_true = y_true.ravel()
+    y_score = y_score.ravel()
+    if y_true.ndim == 1:
+        y_true = y_true.reshape((-1, 1))
+    if y_score.ndim == 1:
+        y_score = y_score.reshape((-1, 1))
+    y_true_c = y_true.take([0], axis=1).ravel()
+    y_score_c = y_score.take([0], axis=1).ravel()
+    precision, recall, pr_thresholds = precision_recall_curve(y_true_c, y_score_c)
+    return (precision, recall, pr_thresholds)
+#-------------------------------------------------------------------------------------------
+def roc_aupr_score(y_true, y_score, average="macro"):
+    def _binary_roc_aupr_score(y_true, y_score):
+        precision, recall, pr_thresholds = precision_recall_curve(y_true, y_score)
+        return auc(recall, precision, reorder=True)
+
+    def _average_binary_score(binary_metric, y_true, y_score, average):  # y_true= y_one_hot
+        if average == "binary":
+            return binary_metric(y_true, y_score)
+        if average == "micro":
+            y_true = y_true.ravel()
+            y_score = y_score.ravel()
+        if y_true.ndim == 1:
+            y_true = y_true.reshape((-1, 1))
+        if y_score.ndim == 1:
+            y_score = y_score.reshape((-1, 1))
+        n_classes = y_score.shape[1]
+        score = np.zeros((n_classes,))
+        for c in range(n_classes):
+            y_true_c = y_true.take([c], axis=1).ravel()
+            y_score_c = y_score.take([c], axis=1).ravel()
+            score[c] = binary_metric(y_true_c, y_score_c)
+        return np.average(score)
+
+    return _average_binary_score(_binary_roc_aupr_score, y_true, y_score, average)
+#------------------------------------------------------------------------------------------
+#Dataset1 (DS1):                                                              
+event_num = 65                                                                
+droprate = 0.3                                                                
+vector_size = 572                                                             
+df_drug = pd.read_pickle('df.pkl')                                            
+conn = sqlite3.connect("event.db")                                                             
+feature_list=df_drug[["side", "target", "enzyme","pathway","smile"]]          
+extraction = pd.read_sql('select * from extraction;', conn)
+mechanism = extraction['mechanism']
+action = extraction['action']
+drugA = extraction['drugA']
+drugB = extraction['drugB']
+#-------------------------------------------------------------------------------------------
+#Dataset2 (DS2):
+event_num = 100
+droprate = 0.3
+vector_size = 1258
+df_drug = pd.read_csv('drug_information_1258.csv')
+df_event = pd.read_csv('drug_interaction.csv')
+feature_list=df_drug[["target", "enzyme","smile"]]
+mechanism = df_event['mechanism']
+action = df_event['action']
+drugA = df_event['drugA']
+drugB = df_event['drugB']
+#--------------------------------------------------------------------------------------------
+seed = 0
+CV = 5
+new_feature, new_label, event_num = prepare(df_drug, feature_list, vector_size, mechanism,action,drugA,drugB)
+all_result, positive_negative, each_result = cross_validation(new_feature, new_label, event_num, seed, CV)
+
